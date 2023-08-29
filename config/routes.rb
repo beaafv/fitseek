82aea2d1a280819be805d960cf0723302fdc9d08
Maariyaah Afzal <maariyaahmafzal@gmail.com>
2023-08-29 12:08:11 +0100

    1

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :workouts, only: [:index, :show] do
+    resources :bookings, only: [:show, :create]
+  end
+  resources :instructors, only: [:show]
+  resources :bookings, only: [:destroy]
+
+  get '/profile', to: "pages#profile", as: :profile
+  get '/dashboard', to: "pages#dashboard", as: :dashboard
 end
