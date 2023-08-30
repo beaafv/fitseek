3d5a420fe8a0e90555ababe8a7342f755cafb77f
Beatriz Vieira <beaferreirav@gmail.com>
2023-08-30 15:11:46 +0100

    searchbar function

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,5 +3,10 @@ class WorkoutsController < ApplicationController
     @workouts = Workout.all
     @users = User.all
     @user = User.find_by(params[:id])
+
+    if params[:query].present?
+      sql_subquery = "category ILIKE :query OR subcategory ILIKE :query"
+      @workouts = @workouts.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 end
