ce0da25c3f756f32fe5c6e6ea8c09d96586e0139
Maariyaah Afzal <maariyaahmafzal@gmail.com>
2023-09-05 18:23:15 +0100

    fixed margin erors

--- db/migrate/20230905120058_change_cost_from_workouts.rb
+++ db/migrate/20230905120058_change_cost_from_workouts.rb
@@ -1,6 +1,5 @@
 class ChangeCostFromWorkouts < ActiveRecord::Migration[7.0]
   def change
-    remove_column :workouts, :cost, :integer
     add_column :workouts, :cost, :integer, default: 0
   end
 end
